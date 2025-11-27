ce457754df961394fd598c812b9142d4706d8cd8
Hacker <1835824864@qq.com>
2025-11-27 20:06:19 +0800

    Site updated: 2025-11-27 20:06:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/19/CentOS7模板机搭建/","2025/04/20/CentOS7配置静态IP/","2025/11/23/Docker2Compose/","2025/11/10/Docker教程/","2023/11/12/Html教程/","2024/10/13/JDBC/","2025/04/18/VMware下载教程/","2025/10/05/Linux常用脚本/","2025/04/18/Linux教程/"];function toRandomPost(){
+var posts=["2025/04/19/CentOS7模板机搭建/","2025/04/20/CentOS7配置静态IP/","2025/11/23/Docker2Compose/","2025/11/10/Docker教程/","2023/11/12/Html教程/","2024/10/13/JDBC/","2025/10/05/Linux常用脚本/","2025/04/18/Linux教程/","2025/04/18/VMware下载教程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
