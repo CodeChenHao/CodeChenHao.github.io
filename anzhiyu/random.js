6fc7a33b2c3a8ea13d35fae21d5329c85f03e067
Hacker <1835824864@qq.com>
2025-11-29 15:27:49 +0800

    Site updated: 2025-11-29 15:27:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/f21b/","posts/422e/","posts/30db/","posts/1f7a/","posts/5d95/","posts/6139/","posts/5c27/","posts/9c04/","posts/20db/","posts/69d9/","posts/b280/","posts/7cd4/","posts/b9a6/","posts/35f5/","posts/9da6/"];function toRandomPost(){
+var posts=["posts/f21b/","posts/422e/","posts/30db/","posts/1f7a/","posts/5d95/","posts/6139/","posts/9c04/","posts/20db/","posts/5c27/","posts/69d9/","posts/b280/","posts/7cd4/","posts/b9a6/","posts/35f5/","posts/9da6/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
